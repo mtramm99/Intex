3115a6b49b5fdb4709c8d10db4c490c7f91872b3
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-04-04 22:27:42 -0600

    DATABASE WORKS

--- Intex/Controllers/HomeController.cs
+++ Intex/Controllers/HomeController.cs
@@ -11,15 +11,17 @@ namespace Intex.Controllers
 {
     public class HomeController : Controller
     {
+        private ICollisionsRepository repo { get; set; }
 
-        public HomeController()
+        public HomeController(ICollisionsRepository temp)
         {
-
+            repo = temp;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var test = repo.Collisions.Where(x => x.CRASH_DATETIME == "2019-02-08T10:56:00.000").ToList();
+            return View(test);
         }
 
         public IActionResult Privacy()
