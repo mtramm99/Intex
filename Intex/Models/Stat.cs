34c4be58f66623b70ed4c75589e18082653e7c2a
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-04-08 00:05:40 -0600

    Trying to do the analytics

---
+++ Intex/Models/Stat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intex.Models
+{
+    public class Stat
+    {
+        public string CityName { get; set; }
+        public int numCrash { get; set; }
+        public string topFactor { get; set; }
+    }
+}
